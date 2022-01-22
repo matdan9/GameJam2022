53593a31ab1354b1a1c32951841e71eaa3b34b00
Rustis7 <rustis7@live.ca>
2022-01-22 01:00:59 -0400

    Finished frost feature and campfire functionnality

--- Assets/Scripts/Frost.cs
+++ Assets/Scripts/Frost.cs
@@ -6,23 +6,25 @@ using UnityEngine.UI;
 public class Frost : MonoBehaviour
 {
     public GameObject frostOverlay;
-    public float frostValue;
-    public bool cold = false;
+    public float multiplier;
+    float frostValue;
+    bool cold = false;
 
     public void Start()
     {
         SetFrost(0);
+        SetMultiplier(1);
     }
 
     public void FixedUpdate()
     {
         frostValue = Mathf.Clamp(frostValue, 0, 255);
         Color32 temp = frostOverlay.GetComponent<RawImage>().color;
-        temp.a = (byte)frostValue;
+        temp.a = (byte)frostValue; //color.a is transparency
         frostOverlay.GetComponent<RawImage>().color = temp;
 
-        if (cold) AddFrost(0.1f);
-        else RemoveFrost(0.1f);
+        if (cold) AddFrost(0.1f * multiplier);
+        else RemoveFrost(0.1f * multiplier);
     }
 
     public void SetFrost(float alpha)
@@ -38,5 +40,15 @@ public class Frost : MonoBehaviour
     public void RemoveFrost(float alpha)
     {
         frostValue -= alpha;
+    }
+
+    public void SetCold(bool status)
+    {
+        cold = status;
+    }
+
+    public void SetMultiplier(float value)
+    {
+        multiplier = value;
     }
 }
